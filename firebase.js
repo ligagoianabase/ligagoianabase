262aa114f213012bd8a5b2523a67a1352e233137
ligagoianabase <ligagoianaoficial@gmail.com>
2026-04-22 10:55:12 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyArj4yRlEl-yibkc0VEmL_4UJpRHHQSc",
-  authDomain: "liga-goiana-base.firebaseapp.com",
-  projectId: "liga-goiana-base",
-  storageBucket: "liga-goiana-base.firebasestorage.app",
-  messagingSenderId: "952863591183",
-  appId: "1:952863591183:web:4eea34f494ba2dd754cc7a"
+  apiKey: "AIzaSyDJ5kD9ywOulWbd-_QBeoc2mmaVfFRBIkM",
+  authDomain: "liga-goiana-base-3e446.firebaseapp.com",
+  projectId: "liga-goiana-base-3e446",
+  storageBucket: "liga-goiana-base-3e446.firebasestorage.app",
+  messagingSenderId: "130295368046",
+  appId: "1:130295368046:web:94d24f980680849677997f"
 };
 
 const app = initializeApp(firebaseConfig);
