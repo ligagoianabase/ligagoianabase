aac264d172c49b0b8edc16b354b213617434e018
ligagoianabase <ligagoianaoficial@gmail.com>
2026-05-06 03:27:41 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+import { 
+  getAuth 
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+
+import { 
+  getFirestore 
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJ5kD9ywOulWbd-_QBeoc2mmaVfFRBIkM",
@@ -14,4 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
