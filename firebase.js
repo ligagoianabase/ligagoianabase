c8db8a521bfbe472a6b4786677a67a5c7a1f1ed0
ligagoianabase <ligagoianaoficial@gmail.com>
2026-05-12 17:57:51 -0300

    Remove storage import and export from firebase.js

--- firebase.js
+++ firebase.js
@@ -8,15 +8,10 @@ import {
   getFirestore 
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDJ5kD9ywOulWbd-_QBeoc2mmaVfFRBIkM",
   authDomain: "liga-goiana-base-3e446.firebaseapp.com",
   projectId: "liga-goiana-base-3e446",
-  storageBucket: "liga-goiana-base-3e446.firebasestorage.app",
   messagingSenderId: "130295368046",
   appId: "1:130295368046:web:94d24f980680849677997f"
 };
@@ -24,7 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
-
-export const storage = getStorage(app);
