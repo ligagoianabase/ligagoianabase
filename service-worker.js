389619d858b66a3fe32b84f6737e6be83b8d2f0a
ligagoianabase <ligagoianaoficial@gmail.com>
2026-05-16 13:14:31 -0300

    Update service worker cache and fetch logic

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,58 @@
-const CACHE_NAME = 'liga-goiana-cache-v1';
+const CACHE_NAME = "liga-goiana-v1";
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo-liga.jfif',
-  '/fundo.png'
+  "/",
+  "index.html",
+  "login.html",
+  "perfil.html",
+  "jogos.html",
+  "sumulas.html",
+  "sumula-offline.html",
+  "campeonatos.html",
+  "times.html",
+  "jogadores.html",
+  "tecnicos.html",
+  "arbitros.html",
+  "assistentes.html",
+  "noticias.html",
+  "firebase.js",
+
+  "logo-liga.jfif",
+  "hero-liga.jpeg",
+  "fundo.png",
+
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
+          if(key !== CACHE_NAME){
             return caches.delete(key);
           }
         })
       );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match("index.html");
     })
   );
 });
