b17df94e6c0316299c5e87d8eafe754d23231823
ligagoianabase <ligagoianaoficial@gmail.com>
2026-05-16 13:15:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,61 @@
-const CACHE_NAME = "liga-goiana-v1";
+const CACHE_NAME = 'liga-goiana-cache-v2';
 
 const urlsToCache = [
-  "/",
-  "index.html",
-  "login.html",
-  "perfil.html",
-  "jogos.html",
-  "sumulas.html",
-  "sumula-offline.html",
-  "campeonatos.html",
-  "times.html",
-  "jogadores.html",
-  "tecnicos.html",
-  "arbitros.html",
-  "assistentes.html",
-  "noticias.html",
-  "firebase.js",
+  '/',
+  '/index.html',
+  '/login.html',
+  '/perfil.html',
+  '/jogos.html',
+  '/sumulas.html',
+  '/sumula-offline.html',
+  '/campeonatos.html',
+  '/times.html',
+  '/jogadores.html',
+  '/tecnicos.html',
+  '/arbitros.html',
+  '/assistentes.html',
+  '/noticias.html',
 
-  "logo-liga.jfif",
-  "hero-liga.jpeg",
-  "fundo.png",
+  '/manifest.json',
+  '/firebase.js',
 
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  '/logo-liga.jfif',
+  '/hero-liga.jpeg',
+  '/fundo.png',
+
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/index.html');
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys.map(key => {
-          if(key !== CACHE_NAME){
+          if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
       );
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    }).catch(() => {
-      return caches.match("index.html");
     })
   );
 });
